fd0e895c48ac49e6d7569b4121438dae954ccc83
ohseungmin <ohmin2886@gmail.com>
2024-01-19 00:03:46 +0900

    calss 작성법의 바닐라 버젼

---
+++ class.js
@@ -0,0 +1,9 @@
+function add(idValue, passwordValue, emailValue) {
+  return {
+    id : idValue,
+    password : passwordValue,
+    email : emailValue
+  }
+}
+
+let result = add("아이디", "비밀번호", "이메일")
