305cc28e0d6f8be07eb86a9eed4d74936eb96c9a
ohseungmin <ohmin2886@gmail.com>
2024-01-19 00:06:02 +0900

    클래스 작성법을 사용한 객체

--- class.js
+++ class.js
@@ -1,7 +1,18 @@
-function add(idValue, passwordValue, emailValue) {
-  this.id = idValue;
-  this.password = passwordValue;
-  this.email = emailValue;
+//function add(idValue, passwordValue, emailValue) {
+//  this.id = idValue;
+//  this.password = passwordValue;
+//  this.email = emailValue;
+//}
+
+//let result = add("아이디", "비밀번호", "이메일")
+
+class add {
+  constructor(idValue, passwordValue, emailValue) {
+    this.id = idValue;
+    this.password = passwordValue;
+    this.email = emailValue;
+  }
 }
 
-let result = add("아이디", "비밀번호", "이메일")
+let result = new add("아이디", "비밀번호", "이메일");
+
